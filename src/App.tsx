93622f3c86aa734417b81f2e38cb360c379d9cbf
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-06-28 21:12:12 -0600

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {json} from "stream/consumers";
 
 function App() {
     const [value, setValue] = useState<number>(0)
 
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('counterKey')
+        /*  let newValue = valueAsString && JSON.parse(valueAsString)*/
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }, [])
+
+    /* useEffect(() => {
+         localStorage.setItem('counterKey', JSON.stringify(value))
+     }, [value])*/
+
     const incHandler = () => {
         setValue(value + 1)
-    }
-    const setToLocalStorage = () => {
-        localStorage.setItem('counterKey', JSON.stringify(value))
-    }
-    const getFromLocalStorage = () => {
-        let valueAsString = localStorage.getItem('counterKey')
-        let newValue = valueAsString && JSON.parse(valueAsString)
-        setValue(newValue)
-    }
-    const clearLocalStorage = () => {
-        localStorage.clear()
-        setValue(0)
+        localStorage.setItem('counterKey', JSON.stringify(value + 1))
     }
 
     return (
         <div className="App">
             <h1>{value}</h1>
             <button onClick={incHandler}>Inc</button>
-            <button onClick={setToLocalStorage}>Set to Local Storage</button>
-            <button onClick={getFromLocalStorage}>Get from local storage</button>
-            <button onClick={clearLocalStorage}>Clear Local Storage</button>
+
         </div>
     );
 }
